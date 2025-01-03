04732855bd8e71c3438d12ddc78194a5d5df0ea5
Sujal Raj <sujalraj611@gmail.com>
2025-01-03 16:01:43 +0530

    updates

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,8 @@ import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 
 const pool = new Pool({
-    connectionString: "postgresql://postgres:admin123@db:5432/mydb"
+    connectionString: process.env.DATABASE_URL || "postgresql://postgres:admin123@db:5432/mydb",
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 export const db = drizzle(pool);
