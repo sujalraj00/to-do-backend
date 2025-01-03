d06de95bdbf8aa9a63f79a0b0cd6cf2789a34904
Sujal Raj <sujalraj611@gmail.com>
2025-01-03 19:15:13 +0530

    port added

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,12 @@ const app = express();
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/tasks", taskRouter);
-
+const PORT = process.env.PORT || 8000;
 app.get("/", (req, res) => {
     res.send("Welcome to my app.!!!!")
 });
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
     console.log("Server started at port : 8000");
 
 });
