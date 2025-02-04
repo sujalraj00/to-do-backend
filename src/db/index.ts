d6373e3a6d3fcf9635fe2cc1c58461f71733d48d
Sujal Raj <sujalraj611@gmail.com>
2025-02-04 17:41:23 +0530

    checking through remote and local

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,17 @@
 import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL || "postgresql://postgres:admin123@db:5432/mydb",
+//     ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+// });
+
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || "postgresql://postgres:admin123@db:5432/mydb",
-    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+
+
+
+    connectionString: "postgresql://postgres:admin123@db:5432/mydb"
 });
+
 
 export const db = drizzle(pool);
